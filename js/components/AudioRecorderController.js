a699103a3612ca205d9383afc3096e2bd489f117
zhangfuwen <zhangfuwen@foxmail.com>
2026-02-01 17:48:13 +0800

    temp

--- js/components/AudioRecorderController.js
+++ js/components/AudioRecorderController.js
@@ -507,18 +507,12 @@ export class AudioRecorderController {
         if (recordBtn) {
             if (this.isRecording) {
                 recordBtn.innerHTML = `
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <rect x="6" y="6" width="12" height="12" stroke-width="2"/>
-                    </svg>
                     Stop Audio Recording
                 `;
                 recordBtn.classList.remove('bg-green-600', 'hover:bg-green-700');
                 recordBtn.classList.add('bg-red-600', 'hover:bg-red-700');
             } else {
                 recordBtn.innerHTML = `
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z" />
-                    </svg>
                     Start Audio Recording
                 `;
                 recordBtn.classList.remove('bg-red-600', 'hover:bg-red-700');
--- js/components/VideoRecorderController.js
+++ js/components/VideoRecorderController.js
@@ -431,18 +431,12 @@ export class VideoRecorderController {
         if (recordBtn) {
             if (this.isRecording) {
                 recordBtn.innerHTML = `
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <rect x="6" y="6" width="12" height="12" stroke-width="2"/>
-                    </svg>
                     Stop Recording
                 `;
                 recordBtn.classList.remove('bg-blue-600', 'hover:bg-blue-700');
                 recordBtn.classList.add('bg-red-600', 'hover:bg-red-700');
             } else {
                 recordBtn.innerHTML = `
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
-                    </svg>
                     Start Recording
                 `;
                 recordBtn.classList.remove('bg-red-600', 'hover:bg-red-700');
